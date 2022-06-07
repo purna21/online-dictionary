a45d2dce59b31e56e8ffe8a2111b2b176637036e
PurnaChandra <mepurnachandra21@gmail.com>
2022-06-07 17:15:08 +0530

    v 1.0

--- script.js
+++ script.js
@@ -25,9 +25,58 @@ searchInput.addEventListener('keydown', (e) => {
             .then((data) => {
                 console.log(data);
 
+                // Assigning the searched word to the Dom element 'word'
+                word.textContent = `${data[0].word}`;
+
+                // Assigning the searched word phonetic to the Dom element 'phonetic'
+                phonetic.textContent = `${data[0].phonetic}`;
+
+                // Assigning the searched word noun to the Dom element 'nounDetails'
 
 
-            });
+                // let test1 = item.meanings[0].partOfSpeech;
+                // console.log(typeof test1);
+
+                if (data[0].meanings[0].partOfSpeech == 'noun') {
+
+                    nounDetails.textContent = data[0].meanings[0].definitions[0].definition;
+
+                } else if (data[1].meanings[0].partOfSpeech == 'noun') {
+
+                    nounDetails.textContent = data[1].meanings[0].definitions[0].definition;
+
+                } else {
+                    nounDetails.textContent = 'Data not found'
+                };
+
+                if (data[0].meanings[2].partOfSpeech == 'adjective') {
+
+                    adjectiveDetails.textContent = data[0].meanings[2].definitions[0].definition;
+
+                } else if (data[1].meanings[2].partOfSpeech == 'adjective') {
+
+                    adjectiveDetails.textContent = data[1].meanings[2].definitions[0].definition;
+
+                } else {
+                    nounDetails.textContent = 'Data not found'
+                };
+
+                if (data[0].meanings[1].partOfSpeech == 'verb') {
+
+                    verbDetails.textContent = data[0].meanings[1].definitions[0].definition;
+
+                } else if (data[1].meanings[1].partOfSpeech == 'verb') {
+
+                    verbDetails.textContent = data[1].meanings[1].definitions[0].definition;
+
+                } else {
+                    nounDetails.textContent = 'Data not found'
+                };
+
+
+
+            })
+            .catch((error) => console.log(error));
 
     };
 
